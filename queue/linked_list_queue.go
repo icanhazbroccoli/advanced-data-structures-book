818a156633c44ea23a10f96be85b8c1b7ee9d727
Oleg Sidorov <me@whitebox.io>
2020-03-08 22:59:19 +0100

    chapter 1.2: implemented linked list based queue
    Signed-off-by: Oleg Sidorov <me@whitebox.io>

---
+++ queue/linked_list_queue.go
@@ -0,0 +1,63 @@
+package queue
+
+type queueNode struct {
+	item Item
+	next *queueNode
+}
+
+type LinkedListQueue struct {
+	insert, remove *queueNode
+}
+
+var _ Queue = (*LinkedListQueue)(nil)
+
+func NewLinkedListQueue() *LinkedListQueue {
+	return &LinkedListQueue{}
+}
+
+func (q *LinkedListQueue) Empty() bool {
+	return q.insert == nil
+}
+
+func (q *LinkedListQueue) Enqueue(item Item) error {
+	tmp := &queueNode{
+		item: item,
+	}
+	if q.insert != nil {
+		q.insert.next = tmp
+		q.insert = tmp
+	} else {
+		q.remove = tmp
+		q.insert = tmp
+	}
+	return nil
+}
+
+func (q *LinkedListQueue) Dequeue() (Item, error) {
+	if q.Empty() {
+		return nil, QueueIsEmptyError
+	}
+	tmp := q.remove
+	q.remove = tmp.next
+	if q.remove == nil {
+		q.insert = nil
+	}
+	return tmp.item, nil
+}
+
+func (q *LinkedListQueue) Peek() (Item, error) {
+	if q.Empty() {
+		return nil, QueueIsEmptyError
+	}
+	return q.remove.item, nil
+}
+
+func (q *LinkedListQueue) Traverse() []Item {
+	res := make([]Item, 0)
+	tmp := q.remove
+	for tmp != nil {
+		res = append(res, tmp.item)
+		tmp = tmp.next
+	}
+	return res
+}
