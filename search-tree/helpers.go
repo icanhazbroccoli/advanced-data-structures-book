433311c9e6b13801c3f8666f83f4422c77d869be
Oleg Sidorov <me@whitebox.io>
2020-03-29 20:54:07 +0200

    chapter 2.8 top-down tree buildup
    Signed-off-by: Oleg Sidorov <me@whitebox.io>

--- search-tree/helpers.go
+++ search-tree/helpers.go
@@ -58,3 +58,45 @@ func MakeTreeBottomUp(list *BinaryTree) *BinaryTree {
 
 	return root
 }
+
+func MakeTreeTopDown(list *BinaryTree) *BinaryTree {
+	type stackItem struct {
+		node1, node2 *BinaryTree
+		number       int
+	}
+	length := 0
+	for tmp := list; tmp != nil; tmp = tmp.right {
+		length++
+	}
+	root := NewBinaryTree()
+	var left, right stackItem
+	current := stackItem{
+		node1:  root,
+		number: length,
+	}
+	stack := []stackItem{current}
+
+	for len(stack) > 0 {
+		current, stack = stack[0], stack[1:]
+		if current.number > 1 {
+			left.node1 = NewBinaryTree()
+			left.node2 = current.node2
+			left.number = current.number / 2
+			right.node1 = NewBinaryTree()
+			right.node2 = current.node1
+			right.number = current.number - left.number
+			current.node1.left = left.node1
+			current.node1.right = right.node1
+			stack = append([]stackItem{left, right}, stack...)
+		} else {
+			current.node1.left = list.left
+			current.node1.key = list.key
+			current.node1.right = nil
+			if current.node2 != nil {
+				current.node2.key = list.key
+			}
+			list = list.right
+		}
+	}
+	return root
+}
--- search-tree/helpers_test.go
+++ search-tree/helpers_test.go
@@ -110,3 +110,109 @@ func TestMakeTreeBottomUp(t *testing.T) {
 		}
 	}
 }
+
+func TestMakeTreeTopDown(t *testing.T) {
+	tests := []struct {
+		input    *BinaryTree
+		expected *BinaryTree
+	}{
+		{
+			input:    nil,
+			expected: NewBinaryTree(),
+		},
+		{
+			input: &BinaryTree{
+				key:   IntKey(10),
+				value: 11,
+			},
+			expected: &BinaryTree{
+				key:   IntKey(10),
+				value: 11,
+			},
+		},
+		{
+			input: &BinaryTree{
+				key:   IntKey(1),
+				value: 11,
+				right: &BinaryTree{
+					key:   IntKey(2),
+					value: 22,
+					right: &BinaryTree{
+						key:   IntKey(3),
+						value: 33,
+						right: &BinaryTree{
+							key:   IntKey(4),
+							value: 44,
+							right: &BinaryTree{
+								key:   IntKey(5),
+								value: 55,
+								right: &BinaryTree{
+									key:   IntKey(6),
+									value: 66,
+									right: &BinaryTree{
+										key:   IntKey(7),
+										value: 77,
+									},
+								},
+							},
+						},
+					},
+				},
+			},
+			expected: &BinaryTree{
+				key: IntKey(5),
+				left: &BinaryTree{
+					key: IntKey(3),
+					left: &BinaryTree{
+						key: IntKey(2),
+						left: &BinaryTree{
+							key:   IntKey(1),
+							value: 11,
+						},
+						right: &BinaryTree{
+							key:   IntKey(2),
+							value: 22,
+						},
+					},
+					right: &BinaryTree{
+						key: IntKey(4),
+						left: &BinaryTree{
+							key:   IntKey(3),
+							value: 33,
+						},
+						right: &BinaryTree{
+							key:   IntKey(4),
+							value: 44,
+						},
+					},
+				},
+				right: &BinaryTree{
+					key: IntKey(7),
+					left: &BinaryTree{
+						key: IntKey(6),
+						left: &BinaryTree{
+							key:   IntKey(5),
+							value: 55,
+						},
+						right: &BinaryTree{
+							key:   IntKey(6),
+							value: 66,
+						},
+					},
+					right: &BinaryTree{
+						key:   IntKey(7),
+						value: 77,
+					},
+				},
+			},
+		},
+	}
+
+	for _, tt := range tests {
+		res := MakeTreeTopDown(tt.input)
+		if !reflect.DeepEqual(res, tt.expected) {
+			t.Errorf("unexpected tree structure:\ngot= %s\nwant=%s",
+				res.Traverse(), tt.expected.Traverse())
+		}
+	}
+}
